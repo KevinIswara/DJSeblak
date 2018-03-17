eb22ee9dc5c8e650a9ff0c86d467c549ffc22974
Kezia Suhendra <keziasuhendra44@gmail.com>
2018-03-17 14:51:54 +0700

    login page

---
+++ Assets/Scripts/Login.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Firebase;
+using Firebase.Auth;
+using Firebase.Unity.Editor;
+
+public class Login : MonoBehaviour {
+	Text email;
+	Text password;
+	Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+
+	void SignIn(){
+		auth.SignInWithEmailAndPasswordAsync (email.text, password.text).ContinueWith (task => {
+			if (task.IsCanceled) {
+				Debug.LogError ("SignInWithEmailAndPasswordAsync was canceled.");
+				return;
+			}
+			if (task.IsFaulted) {
+				Debug.LogError ("SignInWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+				return;
+			}
+
+			Firebase.Auth.FirebaseUser newUser = task.Result;
+			Debug.LogFormat ("User signed in successfully: {0} ({1})",
+				newUser.DisplayName, newUser.UserId);
+		});
+	}
+}
