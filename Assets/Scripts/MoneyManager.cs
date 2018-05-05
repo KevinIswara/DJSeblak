5e0ceb74739e19ce221566f1c8fd6a9211a47998
Roselina Pradjanata <13515034@std.stei.itb.ac.id>
2018-05-05 16:11:52 +0700

    Add money and profile manager

---
+++ Assets/Scripts/MoneyManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+
+public class MoneyManager : MonoBehaviour {
+
+	public string UserId;
+	public int UserMoney;
+	public int UserDiamond;
+	public int AddMoney;
+	public int AddDiamond;
+
+	void Start () {
+		PlayerPrefs.SetInt("Money", "10");
+		PlayerPrefs.SetInt("Diamond", "2");
+		AddMoney = PlayerPrefs.GetInt("Money");
+		AddDiamond = PlayerPrefs.GetInt("Diamond");
+		FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://djseblak-diamondproduction.firebaseio.com/");
+		UserId = PlayerPrefs.GetString("Id");
+		DatabaseReference reference = FirebaseDatabase.DefaultInstance.GetReference("Users").Child(UserId);
+		reference.GetValueAsync().ContinueWith(task => {
+			if (task.IsFaulted) {
+				UserMoney = "0";
+				UserDiamond = "0";
+			}
+			else if (task.IsCompleted) {
+				DataSnapshot snapshot = task.Result;
+				UserMoney = snapshot.Child("Money").Value;
+				UserDiamond = snapshot.Child("Diamond").Value;
+			}
+		});
+		reference.Child("Money").SetValueAsync(UserMoney + AddMoney);
+		reference.Child("Diamond").SetValueAsync(UserDiamond + AddDiamond);
+	}
+	
+}
---
+++ Assets/Scripts/ProfileManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+
+public class ProfileManager : MonoBehaviour {
+
+	public string UserId;
+	public string UserName;
+	public string UserCountry;
+	public string UserJoinDate;
+	public string UserMoney;
+	public string UserDiamond;
+
+	void Start () {
+		FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://djseblak-diamondproduction.firebaseio.com/");
+		PlayerPrefs.SetString("Id", "1");
+		UserId = PlayerPrefs.GetString("Id");
+		DatabaseReference reference = FirebaseDatabase.DefaultInstance.GetReference("Users").Child(UserId);
+		reference.GetValueAsync().ContinueWith(task => {
+			if (task.IsFaulted) {
+				UserName = "User";
+				UserCountry = "Indonesia";
+				UserJoinDate = "January 1, 2018";
+				UserMoney = "0";
+				UserDiamond = "0";
+			}
+			else if (task.IsCompleted) {
+				DataSnapshot snapshot = task.Result;
+				UserName = snapshot.Child("Name").Value;
+				UserCountry = snapshot.Child("Country").Value;
+				UserJoinDate = snapshot.Child("JoinDate").Value;
+				UserMoney = snapshot.Child("Money").Value.ToString();
+				UserDiamond = snapshot.Child("Diamond").Value.ToString();
+			}
+		})
+		.ValueChanged += HandleValueChanged;
+	}
+	
+	void HandleValueChanged(object sender, ValueChangedEventArgs args) {
+		if (args.DatabaseError != null) {
+			Debug.LogError(args.DatabaseError.Message);
+			return;
+		}
+		DataSnapshot snapshot = args.Snapshot;
+		UserName = snapshot.Child("Name").Value;
+		UserCountry = snapshot.Child("Country").Value;
+		UserJoinDate = snapshot.Child("JoinDate").Value;
+		UserMoney = snapshot.Child("Money").Value.ToString();
+		UserDiamond = snapshot.Child("Diamond").Value.ToString();
+	}
+
+}
